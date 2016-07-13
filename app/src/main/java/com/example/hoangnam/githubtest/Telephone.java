ee57cb3e30f5c35775f05c3bdf0614325e3bba4d
Hoang Nam <namhn1495@gmail.com>
2016-07-13 14:35:18 +0700

    Tôi up lên lớp model. User dùng để đóng gói đối tượng người Adapter nó nhiệm vụ đưa hình ảnh listview

---
+++ app/src/main/java/com/example/hoangnam/githubtest/Telephone.java
@@ -0,0 +1,8 @@
+package com.example.hoangnam.githubtest;
+
+/**
+ * Created by Hoang Nam on 7/13/2016.
+ */
+public class Telephone {
+    int number;
+}
---
+++ app/src/main/java/com/example/hoangnam/githubtest/User.java
@@ -0,0 +1,9 @@
+package com.example.hoangnam.githubtest;
+
+/**
+ * Created by Hoang Nam on 7/13/2016.
+ */
+public class User {
+    int id;
+    String name;
+}
